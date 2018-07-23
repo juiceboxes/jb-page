089c09a3c80a415c096fc4574e5f04b489132b37
Ryan Long <rlong@redhat.com>
2018-07-23 12:00:48 -0400

    :boom: adding pretty colors

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+# Font Colors
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+SET='\033[0m'
+
+# Font Styles
+BOLD='\033[1;36m'
+
 # Check if it is a pull request
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo -e "Pull Request, not pushing a build"
+    echo -e "${RED}Pull Request --- Skipping secure vars and deploying${SET}"
+    echo -e "${PURPLE}Once this Pull Request is merged, vars will be active and will be deployed!${SET}"
     exit 0;
 else
+    echo "${GREEN}----- Generating Key -----${SET}"
     openssl aes-256-cbc -K $encrypted_5326641bd546_key -iv $encrypted_5326641bd546_iv -in deploy_key.enc -out deploy_key -d
     chmod 600 deploy_key
     eval `ssh-agent -s`
@@ -13,5 +25,6 @@ fi
 
 # If current branch is master, push to ___
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    echo "${GREEN}----- Master Branch: Deploying to Github Pages Repo -----${SET}"
     .travis/release.sh
 fi
--- .travis/release.sh
+++ .travis/release.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
+# Font Colors
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+SET='\033[0m'
+
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 cd dist
+
+echo "${GREEN}----- Initializing Repo -----${SET}"
 git init
+
+echo "${PURPLE}Setting Travis Login Info${SET}"
 git config --global user.name $COMMIT_AUTHOR_USERNAME
 git config --global user.email $COMMIT_AUTHOR_EMAIL
+
+echo "${GREEN}----- Setting Up Remote Repo -----${SET}"
 git remote add travis-build ${REPO}.git
+
+echo "${PURPLE}Adding dist/${SET}"
 git add .
 git commit -m 'Build by Travis'
 git config --list
+
+echo "${GREEN}----- Pushing to Remote Repo (github.io) -----${SET}"
 git push --force --set-upstream travis-build master
